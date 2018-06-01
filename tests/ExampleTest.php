dd4d9f37d27369efe5d426c7ed17c05466f181c1
Rito eduardo <riedgala@gmail.com>
2018-06-01 17:52:27 -0500

    se agregaron pruebas

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,16 +4,25 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase{
+    
+    use DatabaseTransactions;
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel');
+    public function testBasicExample(){
+         
+        $name = "Eduardo Garcia";
+        $email = "isc.rito.garcia@gmail.com";
+        $user = factory(\App\User::class)->create([
+            'name' => $name,
+            'email' => $email
+        ]);
+         
+        $this->actingAs($user,'api');
+        
+        $this->visit('api/user')->see($name)->see($email);
     }
 }
